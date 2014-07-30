4da3930f7a35fa7e950cf8efcaf130c513efd7ef
Luke Bartel <bartelluke@gmail.com>
2014-07-30 09:18:54 -0600

    area method for rectangle passes

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,2 +1,11 @@
 require 'spec_helper'
 
+describe "Rectangle" do
+  it "returns rectangle area" do
+    rectangle = Rectangle.new(5,10)
+    actual = rectangle.area
+    expected = 50
+    expect(actual).to eq(expected)
+  end
+end
+
