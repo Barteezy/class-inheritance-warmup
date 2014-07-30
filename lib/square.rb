c19283343d468acba726a4fe66a7b29d112c3812
Luke Bartel <bartelluke@gmail.com>
2014-07-30 09:25:31 -0600

    area method and rspec test pass

---
+++ lib/square.rb
@@ -0,0 +1,9 @@
+class Square
+  def initialize(side)
+    @side = side
+  end
+
+  def area
+    @side**2
+  end
+end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,2 +1,10 @@
 require 'spec_helper'
 
+describe "Square" do
+  it "returns area of square" do
+    square = Square.new(5)
+    actual = square.area
+    expected = 25
+    expect(actual).to eq(expected)
+  end
+end
